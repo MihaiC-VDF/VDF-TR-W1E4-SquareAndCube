69acb8178c3c82d4ac5c4c16a9877755d9eda5bf
MihaiC-VDF <mihaicucuianu@gmail.com>
2021-12-09 08:16:11 +0200

    Initial commit

---
+++ src/vdf/company/Main.java
@@ -0,0 +1,13 @@
+package vdf.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        for (int i=0; i<11; i++) {
+            System.out.println(i + " " + i*i + " " + i*i*i );
+        }
+
+    }
+}
